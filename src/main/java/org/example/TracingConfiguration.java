b279bddf2eeb0acb953f282c82871bc8ef602d9c
Mike Hepburn <eformat@gmail.com>
2019-03-24 09:42:56 +1000

    set default envs

--- src/main/java/org/example/TracingConfiguration.java
+++ src/main/java/org/example/TracingConfiguration.java
@@ -13,12 +13,12 @@ public class TracingConfiguration {
      */
     @Bean
     public io.opentracing.Tracer jaegerTracer() {
-
-        System.setProperty("JAEGER_SERVICE_NAME", "helloservice");
-        System.setProperty("JAEGER_REPORTER_LOG_SPANS", "true");
-        System.setProperty("JAEGER_SAMPLER_TYPE", "const");
-        System.setProperty("JAEGER_SAMPLER_PARAM", "1.0");
-        System.setProperty("JAEGER_ENDPOINT", "http://localhost:14268/api/traces"); 
+        
+        System.setProperty("JAEGER_SERVICE_NAME", (System.getenv("JAEGER_SERVICE_NAME") != null? System.getenv("JAEGER_SERVICE_NAME") : "helloservice"));
+        System.setProperty("JAEGER_REPORTER_LOG_SPANS", (System.getenv("JAEGER_REPORTER_LOG_SPANS") != null? System.getenv("JAEGER_REPORTER_LOG_SPANS") : "true"));
+        System.setProperty("JAEGER_SAMPLER_TYPE", (System.getenv("JAEGER_SAMPLER_TYPE") != null? System.getenv("JAEGER_SAMPLER_TYPE") : "const"));
+        System.setProperty("JAEGER_SAMPLER_PARAM", (System.getenv("JAEGER_SAMPLER_PARAM") != null? System.getenv("JAEGER_SAMPLER_PARAM") : "1.0"));
+        System.setProperty("JAEGER_ENDPOINT", (System.getenv("JAEGER_ENDPOINT") != null? System.getenv("JAEGER_ENDPOINT") : "http://localhost:14268/api/traces")); 
         
         try {
             return Configuration.fromEnv().getTracer();
