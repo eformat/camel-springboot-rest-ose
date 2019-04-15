c2b73db4b9a4430b4e26a1918065348981c36f10
Mike Hepburn <eformat@gmail.com>
2019-04-15 15:36:26 +1200

    dont need prom in container

--- src/main/java/org/example/PrometheusConfiguration.java
+++ src/main/java/org/example/PrometheusConfiguration.java
@@ -5,22 +5,21 @@ import io.prometheus.client.hotspot.DefaultExports;
 import io.prometheus.client.spring.boot.SpringBootMetricsCollector;
 import org.springframework.boot.actuate.endpoint.PublicMetrics;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.Collection;
 
-@Configuration
+//@Configuration
+// We do not need this for camel routes as FIS image enables it on 9779
 class PrometheusConfiguration {
 
-    @Bean
+    //@Bean
     SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
         SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(publicMetrics);
         springBootMetricsCollector.register();
         return springBootMetricsCollector;
     }
 
-    @Bean
+    //@Bean
     ServletRegistrationBean servletRegistrationBean() {
         DefaultExports.initialize();
         return new ServletRegistrationBean(new MetricsServlet(), "/prometheus");
